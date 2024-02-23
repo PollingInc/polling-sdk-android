36bc6ed6399506f06f7675529c91889536a7a631
jukeboxplays <jukebox.plays@gmail.com>
2024-02-22 21:37:05 -0300

    WebView settings and url fix to accept parameters and run javascript

--- app/src/main/java/com/polling/sdk/WebViewDialog.java
+++ app/src/main/java/com/polling/sdk/WebViewDialog.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.webkit.WebView;
 
+import android.util.Log;
+import java.net.URLEncoder;
+
 public class WebViewDialog extends Dialog {
 
     public WebViewDialog(Context context) {
@@ -17,6 +20,19 @@ public class WebViewDialog extends Dialog {
         setContentView(R.layout.dialog_webview);
 
         WebView webView = findViewById(R.id.webview);
-        webView.loadUrl("https://demo.polling.com/sdk/available-surveys?customer_id=123&api_key=cli_wZJW1tH39TfUMbEumPLrDy15EXDqJA0a");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+
+        StringBuffer buffer = new StringBuffer("https://demo.polling.com/sdk/available-surveys");
+        buffer.append("?customer_id="+ URLEncoder.encode("123"));
+        buffer.append("&api_key="+URLEncoder.encode("cli_wZJW1tH39TfUMbEumPLrDy15EXDqJA0a"));
+
+        Log.d("TestActivity",buffer.toString());
+
+        webView.loadUrl(buffer.toString());
+
+
+        //webView.loadUrl("https://demo.polling.com/sdk/available-surveys?customer_id=123&api_key=cli_wZJW1tH39TfUMbEumPLrDy15EXDqJA0a");
     }
 }
