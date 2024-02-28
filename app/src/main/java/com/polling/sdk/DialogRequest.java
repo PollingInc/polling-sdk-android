3c1c9ea2efdf6a991fac9b0aad5787996e4a9c67
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-02-28 12:32:12 -0300

    Different types of dialogs + DialogRequest model

---
+++ app/src/main/java/com/polling/sdk/DialogRequest.java
@@ -0,0 +1,10 @@
+package com.polling.sdk;
+
+import android.app.Activity;
+
+public class DialogRequest
+{
+    public Activity activity;
+    public String customerId;
+    public String apiKey;
+}
--- app/src/main/java/com/polling/sdk/WebViewDialogHelper.java
+++ app/src/main/java/com/polling/sdk/WebViewDialogHelper.java
@@ -3,9 +3,31 @@ package com.polling.sdk;
 import android.app.Activity;
 import android.content.Context;
 
+
+/*
+Available surveys
+GET     /api/sdk/surveys/available?customer_id=[ID]&api_key=cli_[KEY]
+WebView https://demo.polling.com/sdk/available-surveys
+
+Specific survey
+GET     /api/sdk/surveys/[UUID]?customer_id={ID}&api_key=cli_[KEY]
+WebView https://demo.polling.com/sdk/survey/[uuid]
+
+Completed surveys:
+GET     /api/sdk/surveys/completed?customer_id=[ID]&api_key=cli_[KEY]
+WebView https://demo-api.polling.com/api/sdk/surveys/completed
+*/
+
 public class WebViewDialogHelper {
 
     public String currentUrl;
+    public DialogRequest dialog;
+
+    public WebViewDialogHelper(DialogRequest dialog)
+    {
+        this.dialog = dialog;
+    }
+
 
     public static void showDialog(Activity activity, String url, String customerId, String apiKey) {
         activity.runOnUiThread(new Runnable() {
@@ -16,19 +38,29 @@ public class WebViewDialogHelper {
         });
     }
 
-    public void showDialog(Activity activity, String customerId, String apiKey)
+    public void showDialog(DialogRequest dialog)
     {
-        showDialog(activity, currentUrl, customerId, apiKey);
+        showDialog(dialog.activity, currentUrl, dialog.customerId, dialog.apiKey);
     }
-
 
     public void availableSurveys()
     {
         this.currentUrl = "https://demo.polling.com/sdk/available-surveys";
+        showDialog(this.dialog);
     }
 
-    public void displaySurvey()
+    //still needs some implementation to work like this, like overriding URL parameters
+    public void singleSurvey()
     {
-        //showDialog(Activity activity);
+        this.currentUrl = "https://demo.polling.com/sdk/survey/";
+        showDialog(this.dialog);
     }
+
+    public void completedSurveys()
+    {
+        this.currentUrl = "https://demo-api.polling.com/api/sdk/surveys/completed";
+        showDialog(this.dialog);
+    }
+
+
 }
