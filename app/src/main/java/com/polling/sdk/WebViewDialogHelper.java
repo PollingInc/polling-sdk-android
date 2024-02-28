c6d39f2e06abd29c5e9a797fb9ba348e0b97892c
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-02-27 22:03:41 -0300

    Some work in progress changes for helper

--- app/src/main/java/com/polling/sdk/WebViewDialogHelper.java
+++ app/src/main/java/com/polling/sdk/WebViewDialogHelper.java
@@ -5,12 +5,30 @@ import android.content.Context;
 
 public class WebViewDialogHelper {
 
-    public static void showDialog(Activity activity) {
+    public String currentUrl;
+
+    public static void showDialog(Activity activity, String url, String customerId, String apiKey) {
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                new WebViewDialog(activity, "", "","").show();
+                new WebViewDialog(activity, url, customerId, apiKey).show();
             }
         });
+    }
+
+    public void showDialog(Activity activity, String customerId, String apiKey)
+    {
+        showDialog(activity, currentUrl, customerId, apiKey);
+    }
+
+
+    public void availableSurveys()
+    {
+        this.currentUrl = "https://demo.polling.com/sdk/available-surveys";
+    }
+
+    public void displaySurvey()
+    {
+        //showDialog(Activity activity);
     }
 }
