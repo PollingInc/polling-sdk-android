08c24d2dcc2b3daf84e55476aec90d1dc1d7d1f2
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-03-06 15:05:59 -0300

    An attempt to emulate BottomSheetFragment with Dialog to use with Activity

---
+++ app/src/main/java/com/polling/sdk/WebViewBottomCustom.java
@@ -0,0 +1,51 @@
+package com.polling.sdk;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+public class WebViewBottomCustom
+{
+    private Dialog dialog;
+    private String url;
+    private DialogRequest dialogRequest;
+
+    public WebViewBottomCustom(Context context, String url, DialogRequest dialogRequest) {
+        this.url = url;
+        this.dialogRequest = dialogRequest;
+        initializeDialog(dialogRequest.activity);
+    }
+
+    private void initializeDialog(Context context) {
+        dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.fragment_bottom_sheet_webview);
+
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            window.setBackgroundDrawableResource(android.R.color.transparent);
+        }
+
+        WebView webView = dialog.findViewById(R.id.webview);
+        String endpoint = dialogRequest.ApplyKeyToURL(url);
+        WebViewConfigs.applyDefault(webView);
+        webView.loadUrl(endpoint);
+    }
+
+    public void show() {
+        if (dialog != null) {
+            dialog.show();
+        }
+    }
+
+    public void dismiss() {
+        if (dialog != null) {
+            dialog.dismiss();
+        }
+    }
+}
+
