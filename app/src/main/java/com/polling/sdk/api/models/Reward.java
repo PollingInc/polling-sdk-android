bfb9df46c1e1da37dab4c60fd4de8bf4209ca042
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-12-09 11:46:59 -0300

    Added extra json field

--- app/src/main/java/com/polling/sdk/api/models/Reward.java
+++ app/src/main/java/com/polling/sdk/api/models/Reward.java
@@ -5,23 +5,20 @@ import com.google.gson.Gson;
 public class Reward {
     private String reward_amount;
     private String reward_name;
+    private String complete_extra_json;
 
     public String getRewardAmount() {
         return reward_amount;
-    }
-
-    public void setRewardAmount(String reward_amount) {
-        this.reward_amount = reward_amount;
     }
 
     public String getRewardName() {
         return reward_name;
     }
 
-    public void setRewardName(String reward_name) {
-        this.reward_name = reward_name;
+    public String getCompleteExtraJson()
+    {
+        return complete_extra_json;
     }
-
 
     public static String serialize(Reward reward) {
         if(reward == null) return null;
