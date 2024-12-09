ebf09ebe27c539625d551c34ae67519acbe5cca0
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-12-09 11:52:03 -0300

    putting back the setters and adding a new one

--- app/src/main/java/com/polling/sdk/api/models/Reward.java
+++ app/src/main/java/com/polling/sdk/api/models/Reward.java
@@ -11,13 +11,25 @@ public class Reward {
         return reward_amount;
     }
 
+    public void setRewardAmount(String reward_amount) {
+        this.reward_amount = reward_amount;
+    }
+
     public String getRewardName() {
         return reward_name;
     }
 
+    public void setRewardName(String reward_name) {
+        this.reward_name = reward_name;
+    }
+
     public String getCompleteExtraJson()
     {
-        return complete_extra_json;
+        return this.complete_extra_json;
+    }
+
+    public void setCompleteExtraJson(String complete_extra_json) {
+        this.complete_extra_json = complete_extra_json;
     }
 
     public static String serialize(Reward reward) {
